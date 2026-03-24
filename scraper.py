cd9f963caf3debb70f99f22b81be19c46b854eb3
oggixtag <andreaoggix@gmail.com>
2026-03-24 15:16:14 +0100

    add feature2

--- scraper.py
+++ scraper.py
@@ -23,6 +23,43 @@ def fetch_data(url):
         print(f"❌ Erreur lors de la récupération : {e}")
     return None
 
+def parse_html(html_content):
+    """
+    Étape 2 : Extraction (Parsing)
+    Analyse le DOM et structure les données en objets (dictionnaires).
+    """
+    try:
+        soup : BeautifulSoup = BeautifulSoup(html_content, 'html.parser')
+        articles = []
+
+        # Extraction ciblée : titleline : C'est le nom de la classe spécifique utilisée par le site Hacker News pour entourer le titre de chaque article.
+        # <span class="titleline"><a href="https://example.com/article1">Titre de l'article 1</a></span>">
+        items = soup.select('.titleline') 
+
+        #boucle d'extraction
+        for item in items:
+            link = item.find('a')
+            # Extraction de la source (site web d'origine) si disponible
+            source_tag = item.find('span', class_='sitebit comhead')
+
+            if link:
+                # Mappage des données en dictionnaire Python
+                object_data = {
+                    "title": link.get_text(strip=True),
+                    "url": link['href'],
+                    "source": source_tag.get_text(strip=True) if source_tag else "N/A"
+                }
+                articles.append(object_data)
+        print(f"✅ Extraction réussie : {len(articles)} articles trouvés.")
+        # Affichage des données extraites
+        print
+        return articles
+
+    except soup.exception.SoupException as e:
+        print(f"❌ Erreur lors de l'analyse du DOM : {e}")
+        return None
+
+
 # --- Point d'entrée du script ---
 if __name__ == "__main__":
     target_url = "https://news.ycombinator.com/"
@@ -36,12 +73,6 @@ if __name__ == "__main__":
         # 🔍 Étape 2 : Extraction
         extracted_data = parse_html(html)
         
-        if extracted_data:
-            # 💾 Étape 3 : Sauvegarde
-            if save_to_json(extracted_data):
-                print(f"✅ Succès ! {len(extracted_data)} objets stockés dans 'data/results.json'")
-                print("💡 Prêt pour une intégration Front-end ou API.")
-        else:
-            print("⚠️ Analyse du DOM terminée : aucune donnée pertinente trouvée.")
+        
     else:
         print("🛑 Arrêt du processus : impossible de récupérer le contenu source.")
